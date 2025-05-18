02aa5cbb8801e1454e74fb1f2c98500b6829db8d
Maarcosfdz <marrcos337@gmail.com>
2025-05-18 15:15:40 +0200

    [CCambio de ordenador] Archivos pre cambio

--- Finstats/Finstats/obj/Debug/net8.0-windows/Finstats.AssemblyInfo.cs
+++ Finstats/Finstats/obj/Debug/net8.0-windows/Finstats.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Finstats")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75c79702b08c09f691e14c2f539b910513505154")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7962696fb11ff013d12e0b75daad4f37f54fe5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Finstats")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Finstats")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
